e122d9f43aec108ea4653f047e72cc90e66605c1
Hiroaki Ninomiya <z13052hn@aiit.ac.jp>
2016-11-24 15:35:43 +0000

    i

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module TochiSlide
 
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
-        origins 'aa'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
