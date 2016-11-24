ca3d9b42af3dd88e88ad33c91864aed1b6fc11e7
Hiroaki Ninomiya <z13052hn@aiit.ac.jp>
2016-11-24 11:21:22 +0000

    f

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module TochiSlide
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'localhost:3000'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
